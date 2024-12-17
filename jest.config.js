66850935c4825217ddd044a3198cd3e3e6d41dee
Solomon kanu <solomoncaster523@gmail.com>
2024-12-17 17:48:14 +0000

    [fix]: deployment

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  testTimeout: 30000, // Set this value higher (default is 5000ms)
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+if (process.env.NODE_ENV === "production") {
+  module.exports = {
+    jest: {
+      collectCoverage: false,
+    },
+  };
+}
+
+
 export default nextConfig;
