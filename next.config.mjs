8eb5c470f7608f069c5b6afa854e651f126f6c3e
Solomon kanu <solomoncaster523@gmail.com>
2024-12-17 17:59:59 +0000

    jgjf

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    jest: {
-      collectCoverage: false,
-    },
-  };
-}
-
 
 export default nextConfig;
