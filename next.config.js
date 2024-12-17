01824ff34bc313661c3dccc5a411db725195662f
Solomon kanu <solomoncaster523@gmail.com>
2024-12-17 18:27:20 +0000

    fix me

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = {
+//     jest: {
+//       collectCoverage: false,
+//     },
+//   };
+// }
+
+
+export default nextConfig;
